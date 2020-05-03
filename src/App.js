56ca924ab3c74c66d937f67b0d1a2eb84e7411a1
Putu Gde Aditya Taguh Widiana <13517032@std.stei.itb.ac.id>
2020-05-03 22:12:28 +0700

    init done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <ParticlesBg type="cobweb" bg={true} />
+      <Container>
+        <Typography component="div" style={{ backgroundColor: '#000000', marginTop: '50vh', opacity:'0.8', padding:'2vh 2vw'}}>
+          <Typography variant="h2" style={{color:"#fff", marginBottom:'10px'}}>
+            Hi, my name is Putu Gde Aditya
+          </Typography>
+          <Typography variant="h3" style={{color:"#fff"}}>
+            <b>Coming soon ...</b>
+          </Typography>
+        </Typography>
+      </Container>
+
+    </React.Fragment>
   );
 }
 
